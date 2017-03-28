5f2f6c35be2c1a2e01ea5578c0fd49e311904347
cab13e <cab13e@acu.edu>
2017-03-28 09:53:01 -0500

    adding everything for 6x6

--- GASudoku/GASudoku/Program.cs
+++ GASudoku/GASudoku/Program.cs
@@ -7,12 +7,28 @@ namespace GASudoku
 {
 	class GA
 	{
+
+		public static void showPuz(int[,] toPrint)
+		{
+				for (int m = 0; m < toPrint.GetLength(0); m++)
+				{
+					for (int j = 0; j < toPrint.GetLength(1); j++)
+						Console.Write(toPrint[m, j] + ",");
+					Console.WriteLine();
+				}
+			Console.WriteLine("fitness " + Darwin(toPrint));
+
+			//Console.WriteLine("\n\n\n\n\n\n\n");
+			//Console.WriteLine(Darwin(pop[0]));
+		}
+
+
 		public static List<int[,]> Genesis(int length)
 		{
 			Random rand = new Random();
 			var pop = new List<int[,]>();
 
-			for (int i = 0; i < 100; i++)
+			for (int i = 0; i < 10; i++)
 			{
 				int[,] chrom = (int[,])_globals._puzzle.Clone();
 
@@ -22,7 +38,7 @@ namespace GASudoku
 					{
 						if (chrom[j, m] == 0)
 						{
-							int next = rand.Next(1,7);
+							int next = rand.Next(1, 7);
 							chrom[j, m] = next;
 						}
 					}
@@ -31,7 +47,7 @@ namespace GASudoku
 				pop.Add(chrom);
 			}
 
-			pop.Sort((a, b) => -1 *  Darwin(a).CompareTo(Darwin(b)));
+			pop.Sort((a, b) => Darwin(a).CompareTo(Darwin(b)));
 
 			//Console.WriteLine("finished Genesis");
 
@@ -46,30 +62,31 @@ namespace GASudoku
 				Console.WriteLine(Darwin(thing));
 			}
 
+			Console.WriteLine("pop count created: " + pop.Count);
 			return pop;
 		}
 
-		public static bool hasMore(int[] arrayToCheck)
+		public static int hasMore(int[] arrayToCheck)
 		{
 			var duplicates = arrayToCheck
 			 .GroupBy(s => s)
 			 .Where(g => g.Count() > 1)
 			 .Select(g => g.Key);
 
-			return (duplicates.Count() > 0);
+			return (duplicates.Count());
 		}
 
 		public static int Darwin(int[,] puz)
 		{
 			int totalVal = 0;
 
-			for (int i = 0; i < puz.GetLength(0); i++)
+			/*for (int i = 0; i < puz.GetLength(0); i++)
 			{
 				for (int j = 0; j < puz.GetLength(1); j++)
 				{
 					totalVal += puz[i, j];
 				}
-			}
+			}*/
 
 			var r1 = new int[] { puz[0, 0], puz[0, 1], puz[0, 2], puz[0, 3], puz[0, 4], puz[0, 5] };
 			var r2 = new int[] { puz[1, 0], puz[1, 1], puz[1, 2], puz[1, 3], puz[1, 4], puz[1, 5] };
@@ -77,6 +94,7 @@ namespace GASudoku
 			var r4 = new int[] { puz[3, 0], puz[3, 1], puz[3, 2], puz[3, 3], puz[3, 4], puz[3, 5] };
 			var r5 = new int[] { puz[4, 0], puz[4, 1], puz[4, 2], puz[4, 3], puz[4, 4], puz[4, 5] };
 			var r6 = new int[] { puz[5, 0], puz[5, 1], puz[5, 2], puz[5, 3], puz[5, 4], puz[5, 5] };
+
 
 			var c1 = new int[] { puz[0, 0], puz[1, 0], puz[2, 0], puz[3, 0], puz[4, 0], puz[5, 0] };
 			var c2 = new int[] { puz[0, 1], puz[1, 1], puz[2, 1], puz[3, 1], puz[4, 1], puz[5, 1] };
@@ -86,15 +104,17 @@ namespace GASudoku
 			var c6 = new int[] { puz[0, 5], puz[1, 5], puz[2, 5], puz[3, 5], puz[4, 5], puz[5, 5] };
 
 			var b1 = new int[] { puz[0, 0], puz[0, 1], puz[0, 2], puz[1, 0], puz[1, 1], puz[1, 2] };
-			var b2 = new int[] { puz[0, 3], puz[0, 4], puz[0, 5], puz[1, 3], puz[1, 4], puz[1, 5] };
-			var b3 = new int[] { puz[2, 0], puz[2, 1], puz[2, 2], puz[3, 0], puz[3, 1], puz[3, 2] };
+			var b2 = new int[] { puz[2, 0], puz[2, 1], puz[2, 2], puz[3, 0], puz[3, 1], puz[3, 2] };
+			var b3 = new int[] { puz[0, 3], puz[0, 4], puz[0, 5], puz[1, 3], puz[1, 4], puz[1, 5] };
 			var b4 = new int[] { puz[2, 3], puz[2, 4], puz[2, 5], puz[3, 3], puz[3, 4], puz[3, 5] };
 			var b5 = new int[] { puz[4, 0], puz[4, 1], puz[4, 2], puz[5, 0], puz[5, 1], puz[5, 2] };
 			var b6 = new int[] { puz[4, 3], puz[4, 4], puz[4, 5], puz[5, 3], puz[5, 4], puz[5, 5] };
 
 
-			if (hasMore(r1) || hasMore(r2) || hasMore(r3) || hasMore(r4) || hasMore(r5) || hasMore(r6) || hasMore(c1) || hasMore(c2) || hasMore(c3) || hasMore(c4) || hasMore(c5) || hasMore(c6) || hasMore(b1) || hasMore(b2) || hasMore(b3) || hasMore(b4) || hasMore(b5) || hasMore(b6))
-				totalVal = 0;
+
+			//if (hasMore(r1) || hasMore(r2) || hasMore(r3) || hasMore(r4) || hasMore(c1) || hasMore(c2) || hasMore(c3) || hasMore(c4) || hasMore(b1) || hasMore(b2) || hasMore(b3) || hasMore(b4) )
+			//	totalVal = 0;
+			totalVal = hasMore(r1) + hasMore(r2) + hasMore(r3) + hasMore(r4) + hasMore(r5) + hasMore(r6) + hasMore(c1) + hasMore(c2) + hasMore(c3) + hasMore(c4) + hasMore(c5) + hasMore(c6) + hasMore(b1) + hasMore(b2) + hasMore(b3) + hasMore(b4) + hasMore(b5) + hasMore(b6);
 
 			return totalVal;
 		}
@@ -104,16 +124,17 @@ namespace GASudoku
 		{
 			Random rand = new Random();
 
-			var cross = rand.Next(0, par1.GetLength(0 + 1));
+			var cross = rand.Next(0, 36);
 
-			var child = new int[par1.GetLength(0), par1.GetLength(1)];
+			int[,] child = (int[,])par1.Clone();
+
+			//var child = new int[par1.GetLength(0), par1.GetLength(1)];
 
 			for (int i = 0; i < child.GetLength(0); i++)
 			{
 				for (int j = 0; j < child.GetLength(1); j++)
 				{
-					
-					if (i < cross)
+					if (i*j < cross)
 						child[i, j] = par1[i, j];
 					else
 						child[i, j] = par2[i, j];
@@ -137,99 +158,144 @@ namespace GASudoku
 
 			var hold = pop[0];
 
-			if (_globals.apocalypseNow != 0)
+			if (_globals.apocalypseNow == 0)
 			{
-				var equal = pop[0].Rank == _globals.survivor.Rank && Enumerable.Range(0, pop[0].Rank).All(dimension => pop[0].GetLength(dimension) == _globals.survivor.GetLength(dimension)) && pop[0].Cast<int>().SequenceEqual(_globals.survivor.Cast<int>());
-
-				if (equal)
-					_globals.apocalypseNow++;
-				else
-					_globals.apocalypseNow = 0;
+				_globals.survivor = hold;
 			}
 
-			foreach(var thing in pop.Skip(1))
-			{
-				var checker = pop[0].Rank == thing.Rank && Enumerable.Range(0, pop[0].Rank).All(dimension => pop[0].GetLength(dimension) == thing.GetLength(dimension)) && pop[0].Cast<int>().SequenceEqual(thing.Cast<int>());
-				if (!checker)
-					return pop;
-			}
+			var equal = pop[0].Rank == _globals.survivor.Rank && Enumerable.Range(0, pop[0].Rank).All(dimension => pop[0].GetLength(dimension) == _globals.survivor.GetLength(dimension)) && pop[0].Cast<int>().SequenceEqual(_globals.survivor.Cast<int>());
+
+			if (equal)
+				_globals.apocalypseNow++;
+			else				
+				_globals.apocalypseNow = 0;
+			//_globals.apocalypseNow++;
+			
+
+			//foreach (var thing in pop.Skip(1))
+			//{
+			//	var checker = pop[0].Rank == thing.Rank && Enumerable.Range(0, pop[0].Rank).All(dimension => pop[0].GetLength(dimension) == thing.GetLength(dimension)) && pop[0].Cast<int>().SequenceEqual(thing.Cast<int>());
+			//	if (!checker)
+			//		return pop;
+			//}
 
 
-			for (int i = 0; i < pop.Count; i++)
+			var newPop = new List<int[,]>();
+			newPop.Add(hold);
+
+			for (int i = 1; i < 10; i++)
 			{
-				var newbie = pop[i];
-				for (int j = 0; j < newbie.GetLength(0); j++)
+				int[,] chrom = (int[,])pop[i].Clone();
+
+				for (int j = 0; j < chrom.GetLength(0); j++)
 				{
-					for (int m = 0; m < newbie.GetLength(1); m++)
+					for (int m = 0; m < chrom.GetLength(1); m++)
 					{
 						if (!_globals._changes[j, m])
 						{
-							if (rand.Next(1, 5) == 1)
-							{
-								newbie[j, m] = rand.Next(1, 7);
-							}
+							int next = rand.Next(1, 7);
+							chrom[j, m] = next;
 						}
 					}
 				}
+				//Console.WriteLine("creating " + chrom);
+				newPop.Add(chrom);
 			}
 
 			_globals.survivor = hold;
 
-			pop.Sort((a, b) => -1 * Darwin(a).CompareTo(Darwin(b)));
+			newPop.Sort((a, b) => Darwin(a).CompareTo(Darwin(b)));
 
-			return pop;
+			//foreach(var thing in pop)
+			//{
+			//	for (int m = 0; m < thing.GetLength(0); m++)
+			//	{
+			//		for (int j = 0; j < thing.GetLength(1); j++)
+			//			Console.Write(thing[m, j] + ",");
+			//		Console.WriteLine();
+			//	}
+			//	Console.WriteLine("fitness " + thing);
+			//}
+
+			return newPop;
 		}
 
 		public static List<int[,]> Life(List<int[,]> pop, Stopwatch time)
 		{
 			Random rand = new Random();
-			//var baseInterval = new TimeSpan(0, 5, 0);
+			var baseInterval = new TimeSpan(0, 10, 0);
 			//int apocalypseNow = 0;
 			int p1, p2, i = 0;
 
 
-			while (Darwin(pop[0]) == 0)
+			while (Darwin(pop[0]) != 0)
 			{
-				p1 = Math.Max(rand.Next(100), rand.Next(100));
-				p2 = Math.Max(rand.Next(100), rand.Next(100));
+				p1 = Math.Min(rand.Next(10), rand.Next(10));
+				p2 = Math.Min(rand.Next(10), rand.Next(10));
+
+				while (p1 == p2)
+					p2 = Math.Min(rand.Next(10), rand.Next(10));
 
 				var par1 = pop[p1];
 				var par2 = pop[p2];
 
 				var child = babies(par1, par2);
 				pop.Add(child);
+				//Console.WriteLine("child created using " + p1 + p2);
 
 
-				if (i % 1000 == 0)
-				{
-					Console.WriteLine("child " + i + " created");
-					Console.WriteLine(Darwin(pop[0]));
-				}
+				pop.Sort((a, b) => Darwin(a).CompareTo(Darwin(b)));
 
-				pop.Sort((a, b) => -1 * Darwin(a).CompareTo(Darwin(b)));
-
-				if (Darwin(pop[0]) != 0)
+				if (Darwin(pop[0]) == 0)
 					return pop;
 				
-				pop.RemoveAt(99);
+				pop.RemoveAt(10);
+				Console.Write(".");
 
-				var equal = pop[0].Rank == pop[pop.Count-1].Rank && Enumerable.Range(0, pop[0].Rank).All(dimension => pop[0].GetLength(dimension) == pop[pop.Count-1].GetLength(dimension)) && pop[0].Cast<int>().SequenceEqual(pop[pop.Count-1].Cast<int>());
+
+				if(rand.Next(1,50) == 1)
+				{
+					int[,] chrom = (int[,])_globals._puzzle.Clone();
+
+					for (int j = 0; j < chrom.GetLength(0); j++)
+					{
+						for (int m = 0; m < chrom.GetLength(1); m++)
+						{
+							if (chrom[j, m] == 0)
+							{
+								int next = rand.Next(1, 7);
+								chrom[j, m] = next;
+							}
+						}
+					}
+					//Console.WriteLine("creating " + chrom);
+					pop.Add(chrom);
+
+					pop.Sort((a, b) => Darwin(a).CompareTo(Darwin(b)));
+					pop.RemoveAt(10);
+				}
+
+				var equal = pop[0].Cast<int>().SequenceEqual(pop[pop.Count-1].Cast<int>());
+
 
 				if (equal)
 				{
-					Apocalypse(pop);
-					Console.WriteLine("apocalypse occurred");
-					Console.Beep();
-					Console.Beep();
+					pop = Apocalypse(pop);
+					//Console.WriteLine("apocalypse occurred");
+					Console.WriteLine(_globals.apocalypseNow);
+
+
+					//Console.Beep();
+					//Console.Beep();
 				}
 
 				i++;
 
-				//if (TimeSpan.Compare(time.Elapsed, baseInterval) == 1)
+				if (TimeSpan.Compare(time.Elapsed, baseInterval) == 1)
+					return pop;
+				//if (_globals.apocalypseNow == 1000)
 				//	return pop;
 
-				if (_globals.apocalypseNow == 5)
-					return pop;
 			}
 			return pop;
 		}
@@ -246,8 +312,8 @@ namespace GASudoku
 		{
 			//GA phase = new GA();
 
-			_globals._puzzle = new int[6, 6];
-			_globals._changes = new bool[6, 6];
+			_globals._puzzle = new int[6,6];
+			_globals._changes = new bool[6,6];
 			_globals.apocalypseNow = 0;
 			for (int i = 0; i < 6; i++)
 			{
@@ -277,6 +343,8 @@ namespace GASudoku
 			_globals._puzzle[4, 5] = 5;
 			_globals._puzzle[5, 1] = 4;
 			_globals._puzzle[5, 3] = 1;
+
+
 
 			_globals._changes[0, 2] = true;
 			_globals._changes[0, 4] = true;
@@ -317,6 +385,25 @@ namespace GASudoku
 			var endOfTime = Life(pop, time);
 			time.Stop();
 
+
+
+
+
+
+			//foreach(var thing in pop)
+			//{
+			//	for (int i = 0; i < thing.GetLength(0); i++)
+			//	{
+			//		for (int j = 0; j < thing.GetLength(1); j++)
+			//			Console.Write(thing[i, j] + ",");
+			//		Console.WriteLine();
+			//	}
+
+			//	Console.WriteLine(Darwin(thing));
+			//}
+
+
+
 			var final = endOfTime[0];
 			for (int i = 0; i < final.GetLength(0); i++)
 			{
@@ -326,6 +413,8 @@ namespace GASudoku
 			}
 
 			Console.WriteLine(Darwin(final));
+
+			Console.WriteLine(time.Elapsed);
 
 			Console.Beep();
 			Console.Beep();
